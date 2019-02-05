590bcaa2dc75fd07213fa6da3f644d493a49769a
vinnyotach7 <vinnyotach7@gmail.com>
2019-02-05 19:32:14 +0300

    add changes

---
+++ js/deprecated.js
@@ -0,0 +1,11 @@
+/* Magic Mirror Deprecated Config Options List
+ *
+ *
+ */
+
+var deprecated = {
+	configs: ["kioskmode"],
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {module.exports = deprecated;}
---
+++ js/utils.js
@@ -0,0 +1,16 @@
+/* exported Utils */
+/* Magic Mirror
+ * Utils
+ */
+
+var colors = require("colors/safe");
+
+var Utils = {
+	colors: {
+		warn: colors.yellow,
+		error: colors.red,
+		info: colors.blue
+	}
+};
+
+if (typeof module !== "undefined") {module.exports = Utils;}
