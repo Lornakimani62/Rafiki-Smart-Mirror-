b79e43fddfc59a337f84d260d166042dd11016ad
lorna <kimanilorna@yahoo.com>
2019-02-09 16:10:23 +0300

    Adds the ability to track objects using open cv

--- ColorDetection.py
+++ ColorDetection.py
@@ -6,7 +6,7 @@ import numpy as np
 # Read the picure - The 1 means we want the image in BGR
 img = cv2.imread('yellow_object.JPG', 1) 
 
-# resize imag to 20% in each axis
+# resize image to 20% in each axis
 img = cv2.resize(img, (0,0), fx=0.2, fy=0.2)
 # convert BGR image to a HSV image
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) 
