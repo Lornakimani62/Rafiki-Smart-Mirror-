ffe02d2576a30d0a1333bdd7781ce2105798971e
vinnyotach7 <vinnyotach7@gmail.com>
2019-02-11 17:48:02 +0300

    this file is used for aunthenticating every module one step at a time

--- tests/configs/modules/calendar/basic-auth.js
+++ tests/configs/modules/calendar/basic-auth.js
@@ -28,7 +28,7 @@ var config = {
 						url: "http://localhost:8010/tests/configs/data/calendar_test.ics",
 						auth: {
 							user: "MagicMirror",
-							pass: "CallMeADog",
+							pass: "python3.6",
 							method: "basic"
 						}
 					}
